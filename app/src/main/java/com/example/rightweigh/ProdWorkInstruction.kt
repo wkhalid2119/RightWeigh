4d4b211eaddbaf7b75d5032cb6e5357215ddf509
Waleed <wkhalid2119@gmail.com>
2020-09-07 13:37:04 -0400

    Added buttons for production work instructions, incoming inspections, process work instructions and added in second view for when the user presses the production work instruction button

---
+++ app/src/main/java/com/example/rightweigh/ProdWorkInstruction.kt
@@ -0,0 +1,11 @@
+package com.example.rightweigh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProdWorkInstruction : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prod_work_instruction)
+    }
+}
